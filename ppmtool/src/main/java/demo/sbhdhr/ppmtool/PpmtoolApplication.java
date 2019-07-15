492a2245763ee24fd9a8b9697f432fa164cd905c
Subhashis Dhar <sbhdhr@gmail.com>
2019-07-15 21:09:25 +0530

    first commit

---
+++ ppmtool/src/main/java/demo/sbhdhr/ppmtool/PpmtoolApplication.java
@@ -0,0 +1,13 @@
+package demo.sbhdhr.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmtoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmtoolApplication.class, args);
+	}
+
+}
